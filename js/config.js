13102509753921ed1a0674e9eb7dbcf0ef089b58
hoangnv9 <hoangnv8@gmail.com>
2025-02-21 09:43:42 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_6H3lym5fP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7o96p0cqsee43fahrmdred5aa7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
